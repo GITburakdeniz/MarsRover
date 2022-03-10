90e121437f9002df92d6526a272e8b57d0bebd6b
Burak Deniz <GITburakdeniz@gmail.com>
2022-03-10 21:09:31 +0300

    added posiiton class

---
+++ NavigationLibrary/Position.cs
@@ -0,0 +1,28 @@
+﻿
+namespace NavigationLibrary
+{
+    public class Position
+    {
+        private int xCor;
+        private int yCor;
+        private Direction direction;
+
+        public int XCordinate 
+        {
+            get { return xCor; } 
+            set { xCor = value; } 
+        }
+
+        public int YCordinate
+        {
+            get { return yCor; }
+            set { yCor = value; }
+        }
+
+        public Direction Direction
+        {
+            get { return direction; }
+            set { direction = value; }
+        }
+    }
+}
