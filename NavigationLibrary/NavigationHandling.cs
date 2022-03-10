5c785731a345df3c57df1566ad9ff2a8bc4befe6
Burak DENIZ <b_urakdeniz@hotmail.com>
2022-03-10 14:29:20 +0300

    Turn check added with test

---
+++ NavigationLibrary/NavigationHandling.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace NavigationLibrary
+{
+    public static class NavigationHandling
+    {
+        public static bool IsValidTurn(this string? str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
+
+            if (Regex.IsMatch(str, @"^[LMR]+$"))
+                return true;
+
+            return false;
+        }
+    }
+}
---
+++ RoverTest/NavigationTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NavigationLibrary;
+
+namespace RoverTest
+{
+    [TestClass] 
+    public class NavigationTest
+    {
+        [TestMethod]
+        public void TestInvalidTurns()
+        {
+            string[] borders = { "LLTLL", "MMRU", "1 0", ".", ";", "rml", "RMLI","L MM", "MMRMM RMRRM" };
+            foreach (var word in borders)
+            {
+                bool result = word.IsValidTurn();
+                Assert.IsFalse(result,
+                      string.Format("Expected for '{0}': false; Actual: {1}",
+                                    word, result));
+            }
+        }
+
+        [TestMethod]
+        public void TestValidTurns()
+        {
+            string[] borders = { "LLLL", "MM", "LMLMLMLMM", "MMRMMRMRRM", "MMRMMRMRRLML" };
+            foreach (var word in borders)
+            {
+                bool result = word.IsValidTurn();
+                Assert.IsTrue(result,
+                      string.Format("Expected for '{0}': true; Actual: {1}",
+                                    word, result));
+            }
+        }
+    }
+}
