1fd56cb26cf96b761224c566c9138b25a7d66227
Burak Deniz <GITburakdeniz@gmail.com>
2022-03-10 21:17:06 +0300

    rename array

--- NavigationLibrary/PositionHandling.cs
+++ NavigationLibrary/PositionHandling.cs
@@ -8,12 +8,12 @@
             if (string.IsNullOrWhiteSpace(str))
                 return false;
 
-            string[] borders = str.Split(" ");
+            string[] positions = str.Split(" ");
 
-            if (borders.Length < 2 || borders.Length > 3)
+            if (positions.Length < 2 || positions.Length > 3)
                 return false;
 
-            if (Int64.TryParse(borders[0], out long xCor) && Int64.TryParse(borders[1], out long yCor))
+            if (Int64.TryParse(positions[0], out long xCor) && Int64.TryParse(positions[1], out long yCor))
             {
                 if (xCor >= 0 && yCor >= 0)
                 {
