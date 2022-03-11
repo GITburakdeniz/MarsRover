d01c056be273362bad677007d3348f914207c365
Burak DENIZ <b_urakdeniz@hotmail.com>
2022-03-11 11:52:41 +0300

    Rover class template

---
+++ NavigationLibrary/Rover.cs
@@ -0,0 +1,33 @@
+﻿
+namespace NavigationLibrary
+{
+    public class Rover
+    {
+        private Position position;
+        private NavigationHandling navigation;
+        private string exploreRoute;
+
+        public Rover(long initialXCor, long initialYCor, Direction initialDirection, string route) 
+        {
+            position = new Position(initialXCor,initialYCor,initialDirection);
+            navigation = new NavigationHandling();
+            exploreRoute = route;
+        }
+
+        public void CreateRoute() 
+        {
+             if (navigation != null && position != null)
+            {
+                if (navigation.IsValidRoute(exploreRoute))
+                {
+                    
+                }
+            }
+        }
+
+        public void Explore()
+        {
+
+        }
+    }
+}
