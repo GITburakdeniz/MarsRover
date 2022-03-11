41ec1914badd3930587478a69adf5afac2681264
Burak Deniz <GITburakdeniz@gmail.com>
2022-03-12 00:46:25 +0300

    input file and misson program added

---
+++ Mission/Mission.cs
@@ -0,0 +1,31 @@
+﻿
+using NavigationLibrary;
+
+class Mission
+{
+
+    void ReadInputFile()
+    {
+        string fileName = System.IO.Path.GetFullPath(Directory.GetCurrentDirectory() + @"\Inputs.txt");
+
+        Console.WriteLine("Your file content is:");
+        using (StreamReader sr = File.OpenText(fileName))
+        {
+            string s = "";
+            while ((s = sr.ReadLine()) != null)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+        Console.ReadKey();
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        Mission mission = new Mission();
+        mission.ReadInputFile();
+
+    }
+}
